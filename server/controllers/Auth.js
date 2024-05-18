dc59ff2230ec26084ab131a1eadea971a06ec597
AzeemKhan17 <azeemdeveloper27@gmail.com>
2024-05-18 23:40:21 +0530

    Project : Authentication Done

--- server/controllers/Auth.js
+++ server/controllers/Auth.js
@@ -1,11 +1,9 @@
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
-const OTP = require("../models/OTP");
 const jwt = require("jsonwebtoken");
-const otpGenerator = require("otp-generator");
-const mailSender = require("../utils/mailSender");
+
 const { passwordUpdated } = require("../mail/templates/passwordUpdate");
-const Profile = require("../models/Profile");
+
 require("dotenv").config();
 
 // Signup Controller for Registering USers
@@ -19,9 +17,7 @@ exports.signup = async (req, res) => {
 			email,
 			password,
 			confirmPassword,
-			accountType,
-			contactNumber,
-			otp,
+			contactNumber
 		} = req.body;
 		// Check if All Details are there or not
 		if (
@@ -29,8 +25,7 @@ exports.signup = async (req, res) => {
 			!lastName ||
 			!email ||
 			!password ||
-			!confirmPassword ||
-			!otp
+			!confirmPassword
 		) {
 			return res.status(403).send({
 				success: false,
@@ -55,46 +50,16 @@ exports.signup = async (req, res) => {
 			});
 		}
 
-		// Find the most recent OTP for the email
-		const response = await OTP.find({ email }).sort({ createdAt: -1 }).limit(1);
-		console.log(response);
-		if (response.length === 0) {
-			// OTP not found for the email
-			return res.status(400).json({
-				success: false,
-				message: "The OTP is not valid",
-			});
-		} else if (otp !== response[0].otp) {
-			// Invalid OTP
-			return res.status(400).json({
-				success: false,
-				message: "The OTP is not valid",
-			});
-		}
-
+		
 		// Hash the password
 		const hashedPassword = await bcrypt.hash(password, 10);
 
-		// Create the user
-		let approved = "";
-		approved === "Instructor" ? (approved = false) : (approved = true);
-
-		// Create the Additional Profile For User
-		const profileDetails = await Profile.create({
-			gender: null,
-			dateOfBirth: null,
-			about: null,
-			contactNumber: null,
-		});
 		const user = await User.create({
 			firstName,
 			lastName,
 			email,
 			contactNumber,
 			password: hashedPassword,
-			accountType: accountType,
-			approved: approved,
-			additionalDetails: profileDetails._id,
 			image: `https://api.dicebear.com/5.x/initials/svg?seed=${firstName} ${lastName}`,
 		});
 
@@ -128,8 +93,7 @@ exports.login = async (req, res) => {
 		}
 
 		// Find user with provided email
-		const user = await User.findOne({ email }).populate("additionalDetails");
-
+		const user = await User.findOne({ email });
 		// If user not found with provided email
 		if (!user) {
 			// Return 401 Unauthorized status code with error message
@@ -142,7 +106,7 @@ exports.login = async (req, res) => {
 		// Generate JWT token and Compare Password
 		if (await bcrypt.compare(password, user.password)) {
 			const token = jwt.sign(
-				{ email: user.email, id: user._id, accountType: user.accountType },
+				{ email: user.email, id: user._id },
 				process.env.JWT_SECRET,
 				{
 					expiresIn: "24h",
@@ -178,53 +142,6 @@ exports.login = async (req, res) => {
 		});
 	}
 };
-// Send OTP For Email Verification
-exports.sendotp = async (req, res) => {
-	try {
-		const { email } = req.body;
-
-		// Check if user is already present
-		// Find user with provided email
-		const checkUserPresent = await User.findOne({ email });
-		// to be used in case of signup
-
-		// If user found with provided email
-		if (checkUserPresent) {
-			// Return 401 Unauthorized status code with error message
-			return res.status(401).json({
-				success: false,
-				message: `User is Already Registered`,
-			});
-		}
-
-		var otp = otpGenerator.generate(6, {
-			upperCaseAlphabets: false,
-			lowerCaseAlphabets: false,
-			specialChars: false,
-		});
-		const result = await OTP.findOne({ otp: otp });
-		console.log("Result is Generate OTP Func");
-		console.log("OTP", otp);
-		console.log("Result", result);
-		while (result) {
-			otp = otpGenerator.generate(6, {
-				upperCaseAlphabets: false,
-			});
-		}
-		const otpPayload = { email, otp };
-		const otpBody = await OTP.create(otpPayload);
-		console.log("OTP Body", otpBody);
-		res.status(200).json({
-			success: true,
-			message: `OTP Sent Successfully`,
-			otp,
-		});
-	} catch (error) {
-		console.log(error.message);
-		return res.status(500).json({ success: false, error: error.message });
-	}
-};
-
 // Controller for Changing Password
 exports.changePassword = async (req, res) => {
 	try {
@@ -262,26 +179,6 @@ exports.changePassword = async (req, res) => {
 			{ password: encryptedPassword },
 			{ new: true }
 		);
-
-		// Send notification email
-		try {
-			const emailResponse = await mailSender(
-				updatedUserDetails.email,
-				passwordUpdated(
-					updatedUserDetails.email,
-					`Password updated successfully for ${updatedUserDetails.firstName} ${updatedUserDetails.lastName}`
-				)
-			);
-			console.log("Email sent successfully:", emailResponse.response);
-		} catch (error) {
-			// If there's an error sending the email, log the error and return a 500 (Internal Server Error) error
-			console.error("Error occurred while sending email:", error);
-			return res.status(500).json({
-				success: false,
-				message: "Error occurred while sending email",
-				error: error.message,
-			});
-		}
 
 		// Return success response
 		return res
---
+++ server/models/User.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    contactNumber: {
+      type: String,
+      required: false,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+    accountType: {
+      type: String,
+      required: true,
+    },
+    token: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", UserSchema);
